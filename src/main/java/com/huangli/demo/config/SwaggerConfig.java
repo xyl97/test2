66660c7242b6bed267fc3d24c5ba1abb48ed1174
xyl <https://github.com/xyl97/97.git>
2023-08-20 19:31:49 +0800

    添加swagger

--- src/main/java/com/huangli/demo/config/SwaggerConfig.java
+++ src/main/java/com/huangli/demo/config/SwaggerConfig.java
@@ -18,7 +18,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-
     @Bean
     public Docket creatApi(){
         return new Docket(DocumentationType.SWAGGER_2)
